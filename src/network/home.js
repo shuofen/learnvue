f30f16e3544260ecab5ca469fa30deb3ac1ddaaa
976197740@qq.com <976197740@qq.com>
2020-05-06 22:54:40 +0800

    bug修复

--- src/network/home.js
+++ src/network/home.js
@@ -2,14 +2,12 @@ import {request, request2} from './request'
 
 export function getHomeMulData() {
   return request({
-    url: "/home/multidata",
-    // method: 'get',
-    // data: {"rows":10,"page":1,"name":null,"areaType":2,"province":null,"city":null}
+    url: "/home/multidata"
   })
 }
 
 export function getGoodsData(type, page) {
-  return request2({
+  return request({
     url: "/home/goods?type=" + type +'&page=' + page
   })
 }
--- src/network/request.js
+++ src/network/request.js
@@ -4,30 +4,11 @@ import axios from 'axios'
 export function request(config) {
   const instance = axios.create({
     // baseURL: 'http://123.207.32.32:8000/',
-    baseURL: 'http://192.168.0.123:3000',
+    // baseURL: 'http://192.168.0.123:3000',
+    baseURL: "http://192.168.1.5:3000",
     //  baseURL: 'http://127.0.0.1:3000',
     //  baseURL: 'http://localhost:3000',
     // timeout: 1000
-  })
- instance.interceptors.request.use(config => {
-  // console.log(config)
-  return config
- },err => {
-   console.log(err)
- })
-
-instance.interceptors.response.use(res => {
-  return res.data
-}, err => {
-  console.log(err)
-})
- return instance(config)
-}
-
-
-export function request2(config) {
-  const instance = axios.create({
-    baseURL: 'http://192.168.0.123:3000',
   })
  instance.interceptors.request.use(config => {
   return config
